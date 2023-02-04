b98407ebf33c571a5e9204983c2e7d1d304b5c0d
ms1720 <ms1720@ic.ac.uk>
2023-02-04 20:27:35 +0000

    fixed xml and fixed dependencies

---
+++ Vibes/vibes/lib/google_maps_in_flutter/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/mia/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/mia/Documents/Vibes/Vibes/vibes/lib/google_maps_in_flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
--- Vibes/vibes/lib/main.dart
+++ Vibes/vibes/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:vibes/directions_model.dart';
 import 'package:vibes/directions_repository.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
