caa41fc902f4b3874ef5d154e9c28ed3f754316e
sam-tackaberry <65713897+sam-tackaberry@users.noreply.github.com>
2023-02-05 10:38:54 +0000

    Update receiver.ino
    Updated receiver code

--- receiver.ino
+++ receiver.ino
@@ -1,30 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#define LED D8
 
 //RECEIVER CODE
 
 WiFiServer wifiServer(80);
 
-const char* ssid = "Iphone (9)"; 
-const char* password = "soManyVibes";
-
-#define LED D7
-
-const char* ssid = "Iphone (9)"; 
-const char* password = "soManyVibes";
-
-typedef struct struct_message {
-  bool switch;
-}
-
-struct_message message;
+const char* ssid = "iPhone (60)"; 
+const char* password = "12345678";
 
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   //idk if we wanna check to bool value here
   digitalWrite(LED, HIGH);
   delay(1000);
   digitalWrite(LED, LOW);
-  delay(1000);
 }
 
 void setup() {
@@ -34,9 +23,10 @@ void setup() {
     delay(1000);
     Serial.println("Connecting..");
   } 
+  pinMode(D8, OUTPUT);
   Serial.print("Connected to WiFi. IP: ");
   Serial.println(WiFi.localIP());
-  Wifi.mode(WIFI_STA); //sets device as wifi station
+  WiFi.mode(WIFI_STA); //sets device as wifi station
   if (esp_now_init() != 0) { //initalises esp-now, which allows callback functions
     Serial.println("Error initalising");
     return;
